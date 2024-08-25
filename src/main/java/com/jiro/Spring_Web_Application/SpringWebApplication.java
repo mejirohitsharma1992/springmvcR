c81d430f4ca05fa0975be0a00a8854239c423361
Rohit sharma <me.jirohitsharma1992@gmail.com>
2024-08-25 17:57:51 +0545

    chapter 2.1

---
+++ src/main/java/com/jiro/Spring_Web_Application/SpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.jiro.Spring_Web_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiro/Spring_Web_Application/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiro.Spring_Web_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
